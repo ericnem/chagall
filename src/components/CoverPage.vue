69855c12803b9dc73bf7e5f60acc2c291e4ab9af
tcohenkl <tal.ck40@gmail.com>
2023-05-11 22:37:39 -0400

    Updated router.js & Working Button

--- src/components/CoverPage.vue
+++ src/components/CoverPage.vue
@@ -1,15 +1,19 @@
 <!-- HTML -->
 <template>
   <div class="coverpage">
-    <button id="startbutton" type="button">Start Game</button>
-  </div>
-  <footer>
-    <p id="footer"> 
-       made by <a href="https://www.linkedin.com/in/tal-cohen-kleinstein-515a12261/" target="_blank"> <strong>tcohenkl</strong></a>
-       and <a href="https://www.linkedin.com/in/eric-nemrodov-44738b241/" target="_blank"><strong>enemrodo</strong></a>
-    </p>
-    <br>
-  </footer>
+    <img id="coverImg" alt="chagall cover" :src="require('@/assets/cover_frame.png')" width="225" height="295">
+      <br> 
+        <br>
+          <button id="startbutton" type="button" @click="$router.push('/game')">Start Game</button>
+            <footer>
+              <br>
+                <p id="footer"> 
+                  made by <a href="https://www.linkedin.com/in/tal-cohen-kleinstein-515a12261/" target="_blank"> <strong>tcohenkl</strong></a>
+                  and <a href="https://www.linkedin.com/in/eric-nemrodov-44738b241/" target="_blank"><strong>ericnem</strong></a>
+                </p>
+              <br>
+           </footer>
+    </div>
 </template>
 
 <!-- JS -->
@@ -26,9 +30,30 @@ export default {
 <!-- CSS -->
 <style scoped>
 
+#coverImg {
+  display:block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 175px;
+  position: relative;
+  top: -200px;
+  animation: flyIn 2.5s ease-out forwards;
+}
+
+ @keyframes flyIn {
+  0% {
+    top: -600px; 
+  }
+  100% {
+    top: 0;
+  }
+}
+
+
 #footer {
-  opacity: 0; 
-  animation: fadeIn 4.75s ease-in forwards; /* Apply the fade-in animation */
+  opacity: 0;
+  top: 30px;
+  animation: fadeIn 4.75s ease-in forwards; 
 }
 
 @keyframes fadeIn {
@@ -40,22 +65,17 @@ export default {
   }
 }
 
-#startbutton{
-  color:black;
-  background-color:#eeba2b;
-  border:#eeba2b;
-  font-family:Verdana;
-  font-weight:bold;
-  font-size:20px;
-  margin-top:30px;
-  width:160px;
-  height:30px;
-  border-radius:20px;
-}
-
 #startbutton {
+  color: black;
+  background-color: #eeba2b;
+  border: #eeba2b;
+  font-family: Verdana;
+  font-weight: bold;
+  font-size: 20px;
+  width: 160px;
+  height: 30px;
+  border-radius: 20px;
   position: relative;
-  bottom: -100px; 
   animation: slideUp 1.55s ease-out forwards;
 }
 
@@ -68,9 +88,10 @@ export default {
   }
 }
 
-#startbutton:hover{
-  background-color:#b99225;
-  border:#b99225;
+#startbutton:hover {
+  background-color: #b99225;
+  border: #b99225;
 }
+
 
 </style>
