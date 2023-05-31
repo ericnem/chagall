a1278711ebe1d4e2fa4bbf76b1dffa475a6b3a21
ericnem <ericnem@outlook.com>
2023-05-31 15:07:05 -0400

    Reverted scripts

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/project name/'
+  transpileDependencies: true
 })
