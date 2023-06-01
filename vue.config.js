d67f7f2dce788970718ae443b36362ae98def061
ericnem <ericnem@outlook.com>
2023-06-01 17:26:15 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/chagall/'
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/chagall/'
   //   : '/'
